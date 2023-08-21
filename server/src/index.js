f928125c976fc645cc187caf5c91f79e203b2f24
Anu Oyebanjo <anuoyebanjodunmi660@gmail.com>
2023-08-21 03:58:07 +0100

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+import { userRouter } from "./routes/users.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter );
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+    "mongodb+srv://anuoyebanjodunmi660:omAKBTU6cmGQf5Kl@recipes.eiv3bio.mongodb.net/recipes?retryWrites=true&w=majority",
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }
+    );
+
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
+
+//Username : anuoyebanjodunmi660
+//pw:  omAKBTU6cmGQf5Kl
